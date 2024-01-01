30da0ef2031c88c4cfb8dc577f6c23bf3e8cf207
Ritika Pal <46473189+Palritika1107@users.noreply.github.com>
2024-01-01 15:54:16 +0530

    Handle CORS

--- skribbl-clone/server/index.js
+++ skribbl-clone/server/index.js
@@ -1,39 +1,19 @@
-// server/index.js
 const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const Game = require('./Game');
-
 const app = express();
-// Middleware to enable CORS
-const enableCORS = (req, res, next) => {
-  // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow any origin (replace * with your specific origin if needed)
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific HTTP methods
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
-
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    // Preflight request. Respond successfully.
-    return res.status(204).end();
-  }
-  console.log(res)
-  // Continue to the next middleware or route handler
-  next();
-};
-
-// Use the CORS middleware
-app.use(enableCORS);
-console.log(app)
+const http = require('http');
 const server = http.createServer(app);
-const io = socketIO(server);
+const Game = require('./Game');
 const PORT = process.env.PORT || 3001;
 
-const game = new Game();
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
 });
+
+server.listen(PORT);
+const game = new Game();
 
 io.on('connection', (socket) => {
   console.log('A user connected');
@@ -68,4 +48,3 @@ function updateGameStatus() {
     io.emit('showStartGameButton', false);
   }
 }
-
